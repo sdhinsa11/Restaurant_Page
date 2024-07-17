7f36b98105c08bdec478f336ebaa6c9f7149e8bc
SDHINSA <sohanad@icloud.com>
2024-07-17 10:07:21 -0600

    Changes in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   mode: 'development',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, './dist'),
   },
 
   plugins:[
